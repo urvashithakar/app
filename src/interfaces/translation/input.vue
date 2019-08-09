998dc222be9c8ca1159ea45227d5c1f1f5e61fd4
Ben Haynes <ben@rngr.org>
2019-08-09 17:32:00 -0400

    revert field note position and update field label styling (#1919)
    * fix offset/padding for permission popover
    
    ideally this popover should be on the right, but we need to deal with the cell padding first
    
    * Set autoHide to false (#1907)
    
    * Translation interface fields blank fix (#1902)
    
    * Fix for blank input-boxes in translation interface
    
    * Property access with lodash-get
    
    * Applied Filter for hidden_browse (#1908)
    
    * Fix regression of #1879 (#1914)
    
    * Update diffing to exclude JSON (#1918)
    
    Fixes #1899
    
    * hover effect and clickable zone
    
    * resolve whitespace issue
    
    * resolve whitespace issue
    
    * revert back to inline notes
    
    * remove extra space below textarea interface
    
    * Increase contrast of notes

--- src/interfaces/translation/input.vue
+++ src/interfaces/translation/input.vue
@@ -79,10 +79,15 @@ export default {
     valuesByLang() {
       if (!this.value) return {};
 
-      return _.keyBy(
-        this.value,
-        this.options.translationLanguageField + "." + this.options.languagesPrimaryKeyField
-      );
+      let firstKey = this.options.translationLanguageField;
+      let secondKey = this.options.languagesPrimaryKeyField;
+      return _(this.value)
+        .map(v => {
+          v[firstKey] = v[firstKey][secondKey] || v[firstKey];
+          return v;
+        })
+        .keyBy(this.options.translationLanguageField)
+        .value();
     },
     fieldManyName() {
       return this.relation.field_many.field;
@@ -141,10 +146,10 @@ export default {
             ];
       } else {
         newValue = this.value.map(translation => {
+          let language = translation[this.options.translationLanguageField];
           if (
-            translation[this.options.translationLanguageField][
-              this.options.languagesPrimaryKeyField
-            ] === this.activeLanguage
+            language === this.activeLanguage ||
+            language[this.options.languagesPrimaryKeyField] === this.activeLanguage
           ) {
             return {
               ...translation,
