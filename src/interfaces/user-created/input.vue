42c0a2309b8eef5bd5f936bb74eb2de48a382950
Ben Haynes <ben@rngr.org>
2019-11-10 12:27:30 -0500

    consistent user_created styles

--- src/interfaces/user-created/input.vue
+++ src/interfaces/user-created/input.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="userInfo" class="user-created">
+  <div v-if="userInfo" class="user-created" :class="{ 'icon-right': iconRight }">
     <v-avatar
       v-if="options.display !== 'name'"
       v-tooltip="options.display === 'avatar' ? displayValue : null"
@@ -8,15 +8,18 @@
       :src="src"
       :alt="displayValue"
     ></v-avatar>
-    <span v-if="options.display !== 'avatar'" class="label gray">
+    <span v-if="options.display !== 'avatar'" class="label">
       {{ displayValue }}
     </span>
+    <v-icon v-if="iconRight" :name="iconRight" />
   </div>
-  <div v-else-if="newItem" class="gray">
+  <div v-else-if="newItem" class="user-created placeholder" :class="{ 'icon-right': iconRight }">
     {{ $t("interfaces-user-created-you") }}
+    <v-icon v-if="iconRight" :name="iconRight" />
   </div>
-  <div v-else class="gray">
+  <div v-else class="user-created placeholder" :class="{ 'icon-right': iconRight }">
     {{ $t("interfaces-user-created-unknown") }}
+    <v-icon v-if="iconRight" :name="iconRight" />
   </div>
 </template>
 
@@ -25,6 +28,12 @@ import mixin from "@directus/extension-toolkit/mixins/interface";
 
 export default {
   mixins: [mixin],
+  props: {
+    iconRight: {
+      type: String,
+      default: "account_box"
+    }
+  },
   computed: {
     userInfo() {
       if (!this.value) return null;
@@ -50,10 +59,11 @@ export default {
 
 <style lang="scss" scoped>
 .user-created {
+  position: relative;
   border: var(--input-border-width) solid var(--input-border-color);
   border-radius: var(--border-radius);
   height: 44px;
-  padding: 4px;
+  padding: 4px 10px;
   display: flex;
   align-items: center;
   background-color: var(--input-background-color-disabled);
@@ -66,7 +76,15 @@ export default {
   display: inline-block;
   margin-left: 12px;
 }
-.gray {
-  color: var(--blue-grey-600);
+.placeholder {
+  color: var(--input-placeholder-color);
+}
+.icon-right i {
+  position: absolute;
+  top: 50%;
+  color: var(--input-icon-color);
+  transform: translateY(-50%);
+  font-size: 24px;
+  right: 10px;
 }
 </style>
