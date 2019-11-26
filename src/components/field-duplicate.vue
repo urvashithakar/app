f39cbb636cb06a34d6c60764e8a5db109c64a9ef
rijkvanzanten <rijkvanzanten@me.com>
2019-11-26 17:06:52 -0500

    Fix field duplicate modal

--- src/components/field-duplicate.vue
+++ src/components/field-duplicate.vue
@@ -1,6 +1,8 @@
 <template>
   <v-modal
-    :title="$t('duplicating_field') + ': ' + $helpers.formatField(field.field, field.collection)"
+    :title="
+      $t('duplicating_field') + ': ' + $helpers.formatField(fieldInfo.field, fieldInfo.collection)
+    "
     :buttons="buttons"
     @save="saveField()"
     @close="$emit('close')"
